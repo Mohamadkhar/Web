ab2fd2ff32f78a06e0f13f2a70fb71cb9feecd5c
Mohamad kha <Mohammed.kharboutli@gmail.com>
2021-10-14 15:07:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//let isApproved = true;
+//let Name = undefined;
+//let selectedColor = null;
+// console.log(firstName, Name, age);
+/*function greet(name, lastName) {
+    console.log('Hello ' + name + lastName);
+}
+greet  ('john', ' Haiko');
+greet ('Mary', ' Muller');*/
+
+
+//document.getElementById("count.li").innerText = 6
+/*let myAge = 26, humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge)
+*/
+
+function increment(){
+    console.log("The button was clicked")
+}
+
+function countdown(){
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+}
+
+countdown()
+
+countdown()
